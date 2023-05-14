0cbde5c650bc7dfcee71ba8bbeae061e459a0b14
Sergio <63809662+SergioCrespillo@users.noreply.github.com>
2023-05-14 12:15:15 +0200

    Create pie.php

---
+++ estructura/pie.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<link rel="stylesheet" href="http://localhost/TFG/css/estilo_bancoTFG.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800&display=swap" rel="stylesheet"> 
+	</head>
+	<body>
+		<footer>
+        <div class="contenedor-footer">
+            <div class="content-foo">
+                <h4>¡AVISO!</h4>
+                <p>Esta página web fue desarollada por alumnos de la Universidad Complutene de Madrid como TFG.</p><p>Es para un uso exclusivamente educativo.</p>
+            </div>
+        </div>
+        <h2 class="titulo-final">&copy; Pie de página - BANCO TFG</h2>
+    </footer>
+	</body>
+	
+</html>
